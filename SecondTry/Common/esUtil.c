10cc0d7c6eb93a8af7c78a77ba7d6507dd06f863
Joel <tentwelfths@gmail.com>
2016-09-08 11:36:49 -0700

    only a couple more

--- SecondTry/Common/esUtil.c
+++ SecondTry/Common/esUtil.c
@@ -170,7 +170,7 @@ EGLBoolean WinCreate(ESContext *esContext, const char *title)
          
    dispman_element = vc_dispmanx_element_add ( dispman_update, dispman_display,
       0/*layer*/, &dst_rect, 0/*src*/,
-      &src_rect, DISPMANX_PROTECTION_NONE, 0 /*alpha*/, 0/*clamp*/, 0/*transform*/);
+      &src_rect, DISPMANX_PROTECTION_NONE, 0 /*alpha*/, 0/*clamp*/, DISPMANX_NO_ROTATE/*transform*/);
       
    nativewindow.element = dispman_element;
    nativewindow.width = display_width;
