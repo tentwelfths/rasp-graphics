951f86107de90a8855b72f9025452f76fc05fc8f
Joel <tentwelfths@gmail.com>
2016-09-10 14:54:58 -0700

    debuetxt

--- ThirdTry/GraphicsSystem.cpp
+++ ThirdTry/GraphicsSystem.cpp
@@ -263,6 +263,7 @@ void GraphicsSystem::Draw()
     glUniform1i ( Texture, 0 );
 
     glDrawElements ( GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, indices_ );
+    std::cout<<"err: "<<glGetError()<<std::endl;
   }
   eglSwapBuffers(eglDisplay, eglSurface);
 }
