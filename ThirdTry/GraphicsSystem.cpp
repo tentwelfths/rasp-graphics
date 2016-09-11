78fe56ed8d56d16e1bbcacbaae46435c8def3579
Joel <tentwelfths@gmail.com>
2016-09-11 12:20:15 -0700

    them views of fields

--- ThirdTry/GraphicsSystem.cpp
+++ ThirdTry/GraphicsSystem.cpp
@@ -231,7 +231,7 @@ void GraphicsSystem::Draw()
   
   // Clear the color buffer
   glClear ( GL_COLOR_BUFFER_BIT );
-  
+  float fov = 45.f;
   // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
   glm::mat4 Projection = glm::perspective(fov, 4.0f / 3.0f, 0.1f, 100.0f);
   //glm::mat4 Projection = glm::ortho(-4, 4, 4, -4);
